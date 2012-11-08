828083be5a76ea2bdcb12e19ba7a30335cbd3010
Leonardo Villani <informatica@villani.eti.br>
2012-11-08 14:05:44 -0200

    Início do projeto - Criado objeto BuilderLog para registrar os eventos do experimento. - Acrescentando planejamento do projeto (projeto.docx)

---
+++ src/villani/eti/br/Experiments01.java
@@ -0,0 +1,17 @@
+package villani.eti.br;
+
+public class Experiments01 {
+	
+	private static LogBuilder log;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		log = new LogBuilder("Experiments01.log");
+		log.write("Iniciando experimento 1.3...");
+
+	}
+
+}
