a1a9f5a9877c6a673f35c09fcaa86aaec81707ae
villani <informatica@villani.eti.br>
2012-12-06 18:31:07 -0200

    Alteração em Classificadores.java e resultados da execução 07 e 08 Execução 07 - 2000 imagens - 100 kp - 100sift - 20folds - Estouro de memória na 2ª partição da validação cruzada do processo de avaliação do classificador BRkNN.
    Execução 08 - 2000 imagens - 100 kp - 100sift - 10folds
    Hamming Loss do MLkNN: 0,0029±0,0001
    Hamming Loss do BRkNN: 0,0029±0,0001
    
    DISCUSSÕES
    Os classificadores obtiveram o mesmo valor de Hamming loss na última
    execução. No entanto, para outras medidas, o desempenho de MLkNN
    continua superior (ver arquivo ".results"), mas execução da avaliação
    cruzada de BRkNN foi sutilmente mais rápida que MLkNN.
    
    CONCLUSÕES
    A diferença de desempenho entre os classificadores continua diminuindo
    conforme o número de amostras aumentam. Um estouro de memória ocorria ao
    avaliar a 2ª partição do processo de validação cruzada do 2º
    classificador. O escopo dos classificadores instanciados foi alterado
    para verificar se um melhor consumo de memória era realizado. A execução
    08 mostrou que a mudança foi aceita com sucesso. Pretende-se na execução
    09 aumentar em 50% o número de amostra para verificar se o hardware
    utilizado suporta o experimento e se o desempenho de BRkNN supera o de
    MLkNN. O planejamento de experiments02 já foi iniciado e pretende-se
    investigar os extratores implementados em "jfeaturelib".

--- src/villani/eti/br/Classificadores.java
+++ src/villani/eti/br/Classificadores.java
@@ -24,19 +24,11 @@ public class Classificadores {
 		Classificadores.entradas = entradas;
 		dataset = Classificadores.entradas.get("dataset");
 		folds = Integer.parseInt(Classificadores.entradas.get("folds"));
-		
+
 
 	}
 
 	public static void avalia(MultiLabelInstances instanciasML) {
-		log.write("- Instanciando classificadores multirrótulo");
-
-		log.write("- Instanciando MLkNN");
-		MLkNN classificador01 = new MLkNN();
-
-		log.write("- Instanciando BRkNN");
-		BRkNN classificador02 = new BRkNN(10);
-
 		log.write("- Instanciando avaliador");
 		Evaluator avaliador = new Evaluator();
 		int numFolds = folds;
@@ -45,20 +37,37 @@ public class Classificadores {
 		MultipleEvaluation avaliacao;
 		FileWriter resultados;
 		try {
+
 			resultados = new FileWriter(dataset + ".results");
-			log.write("- Avaliando classificador " + classificador01.getClass());
-			avaliacao = avaliador.crossValidate(classificador01, instanciasML, numFolds);
-			resultados.write("RESULTADOS DO CLASSIFICADOR " + classificador01.getClass());
-			resultados.write("\n=========================================\n\n");
-			resultados.write(avaliacao.toString());
-			resultados.write("\n=========================================\n\n");
-			log.write("- Avaliando classificador " + classificador02.getClass());
-			avaliacao = avaliador.crossValidate(classificador02, instanciasML, numFolds);
-			resultados.write("RESULTADOS DO CLASSIFICADOR " + classificador02.getClass());
-			resultados.write("\n=========================================\n\n");
-			resultados.write(avaliacao.toString());
-			resultados.write("\n=========================================\n\n");
+
+			try{
+				log.write("- Instanciando MLkNN");
+				MLkNN classificador01 = new MLkNN();
+				log.write("- Avaliando classificador " + classificador01.getClass());
+				avaliacao = avaliador.crossValidate(classificador01, instanciasML, numFolds);
+				resultados.write("RESULTADOS DO CLASSIFICADOR " + classificador01.getClass());
+				resultados.write("\n=========================================\n\n");
+				resultados.write(avaliacao.toString());
+				resultados.write("\n=========================================\n\n");
+			} catch(Exception e){
+				log.write("- Falha ao avaliar o classificador 01: " + e.getMessage());
+			}
+
+			try{
+				log.write("- Instanciando BRkNN");
+				BRkNN classificador02 = new BRkNN(10);
+				log.write("- Avaliando classificador " + classificador02.getClass());
+				avaliacao = avaliador.crossValidate(classificador02, instanciasML, numFolds);
+				resultados.write("RESULTADOS DO CLASSIFICADOR " + classificador02.getClass());
+				resultados.write("\n=========================================\n\n");
+				resultados.write(avaliacao.toString());
+				resultados.write("\n=========================================\n\n");
+			} catch(Exception e){
+				log.write("- Falha ao avaliar o classificador 02: " + e.getMessage());
+			}
+
 			resultados.close();
+
 		} catch (Exception e) {
 			log.write("- Falha ao instancias registradores: " + e.getMessage());
 		}
